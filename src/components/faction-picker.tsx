f3bccf569a6f1ddef3d039fefe5b06981d2d510b
Kurtis Blankenship <kblankenship1989@gmail.com>
2021-02-14 21:06:15 -0600

    more changes

--- src/components/faction-picker.tsx
+++ src/components/faction-picker.tsx
@@ -1,44 +1,36 @@
 import React from 'react';
 import {default as DropDownPicker} from 'react-native-dropdown-picker';
-import {getColorScheme} from '../helpers/getColorScheme';
-import {appStyles} from '../../styles';
-import {View} from 'react-native';
-import {Icon} from 'react-native-elements';
 import {FactionPickerProps} from '../types/components/props';
-import {factions} from '../types/consts';
+import {Factions, factions} from '../types/consts';
 
-const colorScheme = getColorScheme();
-const styles = appStyles(colorScheme);
+type LocalItemType = {
+    label: Factions,
+    value: Factions,
+    icon?: () => JSX.Element
+}
 
-const dropdwonValues = factions.map((faction) => ({
+const dropdwonValues : LocalItemType[] = factions.map((faction) => ({
     label: faction,
     value: faction
 }));
 
 export const FactionPicker : React.FC<FactionPickerProps> = ({selectedFaction, onChange}) => {
-    const onValueChange = (item : any, index : number) : void => {
-        onChange(factions[index]);
+    const onValueChange = (item : LocalItemType) : void => {
+        onChange(item.value);
     };
 
     return (
-        <View style={styles.pickerRow}>
-            <Icon
-                size={18}
-                name={'sword-cross'}
-                type={'material-community'}
-            />
-            <DropDownPicker
-                items={dropdwonValues}
-                defaultValue={selectedFaction || factions[0]}
-                containerStyle={{height: 40}}
-                style={{backgroundColor: '#fafafa'}}
-                itemStyle={{
-                    justifyContent: 'flex-start'
-                }}
-                dropDownStyle={{backgroundColor: '#fafafa'}}
-                onChangeItem={onValueChange}
-            />
-        </View>
+        <DropDownPicker
+            items={dropdwonValues}
+            defaultValue={selectedFaction || factions[0]}
+            containerStyle={{height: 40}}
+            style={{backgroundColor: '#fafafa'}}
+            itemStyle={{
+                justifyContent: 'flex-start'
+            }}
+            dropDownStyle={{backgroundColor: '#fafafa'}}
+            onChangeItem={onValueChange}
+        />
     );
 };
 
