7c90686d52d3122ee413744a7b12176a5b4a5161
Kurtis Blankenship <BlankenshipKurtisA@JohnDeere.com>
2021-02-22 16:41:35 -0600

    rebuild state

--- src/redux/reducers/selected-order-of-battle-id.ts
+++ src/redux/reducers/selected-order-of-battle-id.ts
@@ -1,5 +1,8 @@
-import {AnyAction} from 'redux';
+import {SelectOrderOfBattleAction} from '../action-creators/orders-of-battle';
+import {OrderOfBattleActions} from '../action-list';
 
-export const selectedOrderOfBattleId = (state: string | null = null, action : AnyAction) : string | null => {
-    return state;
+export const selectedOrderOfBattleId = (state: string | null = null, action : SelectOrderOfBattleAction) : string | null => {
+    return action.type === OrderOfBattleActions.SELECT_ORDER_OF_BATTLE ?
+        action.payload.selectedOrderOfBattleId :
+        state;
 };
